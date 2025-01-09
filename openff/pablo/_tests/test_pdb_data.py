06492e9432f7a37a0d18679a410875c39610605c
Josh Mitchell <yoshanuikabundi@gmail.com>
2025-01-09 17:50:33 +1100

    Tweaking test

--- openff/pablo/_tests/test_pdb_data.py
+++ openff/pablo/_tests/test_pdb_data.py
@@ -6,4 +6,5 @@ from openff.pablo._pdb_data import PdbData
 
 @pytest.mark.parametrize("pdbfile", ["data/5ap1_prepared.pdb"])
 def test_can_load_pdb_file_as_data(pdbfile: str):
-    PdbData.from_file(resource_filename(__name__, pdbfile))
+    data = PdbData.from_file(resource_filename(__name__, pdbfile))
+    assert len(data.name) == 53520
