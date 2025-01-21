ae26bab2b0c092df1c11686008efb6d4f43faf22
Josh Mitchell <yoshanuikabundi@gmail.com>
2025-01-21 11:35:02 +1100

    Test _utils module

--- openff/pablo/_utils.py
+++ openff/pablo/_utils.py
@@ -62,6 +62,14 @@ def with_neighbours(
     iterable: Iterable[T],
     default: U = None,
 ) -> Iterator[tuple[T | U, T, T | U]]:
+    """Return each element of the iterable with its neighbours.
+
+        abcd -> _ab, abc, bcd, cd_
+
+    The middle element of the tuple is the current element. Missing neighbours
+    are set to ``default``. The resulting sequence has the same length as the
+    original iterable.
+    """
     iterator = iter(iterable)
 
     pred: T | U = default
@@ -113,6 +121,15 @@ def dec_hex(s: str) -> int:
         "A00F" -> 10015
         "A010" -> 10016
         ...
+        "FFFF" -> 34575
+
+    Strings that can be interpreted as hex but do not have a leading hex digit
+    greater than 9 are a value error, as are strings that cannot be interpreted
+    as either decimal or hexadecimal integers:
+
+        "10A2" -> ValueError
+        " 2.3" -> ValueError
+        "hiya" -> ValueError
     """
 
     try:
@@ -121,6 +138,8 @@ def dec_hex(s: str) -> int:
         n = len(s)
         parsed_as_hex = int(s, 16)
         smallest_hex: int = 0xA * 16 ** (n - 1)
+        if parsed_as_hex < smallest_hex:
+            raise ValueError("hex values must have leading digit greater than 9")
         largest_dec: int = 10**n - 1
         return parsed_as_hex - smallest_hex + largest_dec + 1
 
@@ -168,7 +187,6 @@ def assign_stereochemistry_from_3d(molecule: MoleculeLike):
             # SimpleMolecules do not store stereo info
             return
 
-        # TODO: Assign bonds as well
         rdmol = molecule.to_rdkit()
         AssignStereochemistryFrom3D(rdmol, confId=0, replaceExistingTags=True)
 
