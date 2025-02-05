3b79c236c6cb5f4e177cbc7aafb09df19938344c
Josh Mitchell <yoshanuikabundi@gmail.com>
2025-02-05 11:23:48 +1100

    Simplify atom ordering warning

--- openff/pablo/_pdb.py
+++ openff/pablo/_pdb.py
@@ -347,10 +347,9 @@ def topology_from_pdb(
     topology.set_positions(positions[topology_pdb_indices])
     if topology_pdb_indices != list(range(n)):
         warnings.warn(
-            "Due to limitations of different topology representations, I was"
-            + " unable to preserve the atom indices from the input PDB. The"
-            + " atoms in this topology will not be in same order as those in"
-            + " PDB file",
+            "Input PDB has an atom ordering that cannot be represented in an"
+            + " OpenFF Topology. The atoms in this topology will not be in same"
+            + " order as those in PDB file",
         )
 
     if set_stereochemistry_from_3d:
