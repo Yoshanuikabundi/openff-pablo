a5d6e657a21617e741629f2f42920f066253e296
Josh Mitchell <yoshanuikabundi@gmail.com>
2025-02-18 18:09:52 +1100

    Wrap draw_molecule in no type check

--- openff/pablo/_utils.py
+++ openff/pablo/_utils.py
@@ -329,154 +329,159 @@ def draw_molecule(
         When an atom or bond in highlight_atoms or highlight_bonds is missing
         from the image, including when it is present in the molecule but hidden.
     """
-    # We're working in RDKit
-    try:
-        rdmol = rdkit.Chem.rdchem.Mol(molecule.to_rdkit())
-    except AttributeError:
-        rdmol = rdkit.Chem.rdchem.Mol(molecule)
 
-    # Process color_by_element argument
-    if color_by_element is None:
-        color_by_element = highlight_atoms is None and highlight_bonds is None
+    @no_type_check
+    def inner():
+        # We're working in RDKit
+        try:
+            rdmol = rdkit.Chem.rdchem.Mol(molecule.to_rdkit())
+        except AttributeError:
+            rdmol = rdkit.Chem.rdchem.Mol(molecule)
 
-    if color_by_element:
-        set_atom_palette = lambda draw_options: draw_options.useDefaultAtomPalette()
-    else:
-        set_atom_palette = lambda draw_options: draw_options.useBWAtomPalette()
+        # Process color_by_element argument
+        if color_by_element is None:  # noqa: F823
+            color_by_element = highlight_atoms is None and highlight_bonds is None
 
-    # Process explicit_hydrogens argument
-    # If we need to remove atoms, create a map from the original indices to the
-    # new ones.
-    if explicit_hydrogens is None:
-        idx_map = {i: i for i in range(rdmol.GetNumAtoms())}
-    elif explicit_hydrogens:
-        idx_map = {i: i for i in range(rdmol.GetNumAtoms())}
-        rdmol = rdkit.Chem.AddHs(rdmol, explicitOnly=True)
-    else:
-        idx_map = {
-            old: new
-            for new, old in enumerate(
-                a.GetIdx()
-                for a in rdmol.GetAtoms()  # type: ignore
-                if a.GetAtomicNum() != 1 and a.GetMass() != 1
-            )
-        }
-        rdmol = rdkit.Chem.RemoveHs(rdmol, updateExplicitCount=True)
+        if color_by_element:
+            set_atom_palette = lambda draw_options: draw_options.useDefaultAtomPalette()
+        else:
+            set_atom_palette = lambda draw_options: draw_options.useBWAtomPalette()
 
-    # Process highlight_atoms argument for highlightAtoms and highlightAtomColors
-    # highlightAtoms takes a list of atom indices
-    # highlightAtomColors takes a mapping from atom indices to colors
-    if highlight_atoms is None:
-        highlight_atoms = []
-        highlight_atom_colors = None
-    elif isinstance(highlight_atoms, dict):
-        highlight_atom_colors = {
-            idx_map[i]: tuple(c) for i, c in highlight_atoms.items() if i in idx_map
-        }
-        highlight_atoms = list(highlight_atoms.keys())
-    else:
-        highlight_atoms = [idx_map[i] for i in highlight_atoms if i in idx_map]
-        highlight_atom_colors = None
+        # Process explicit_hydrogens argument
+        # If we need to remove atoms, create a map from the original indices to the
+        # new ones.
+        if explicit_hydrogens is None:
+            idx_map = {i: i for i in range(rdmol.GetNumAtoms())}
+        elif explicit_hydrogens:
+            idx_map = {i: i for i in range(rdmol.GetNumAtoms())}
+            rdmol = rdkit.Chem.AddHs(rdmol, explicitOnly=True)
+        else:
+            idx_map = {
+                old: new
+                for new, old in enumerate(
+                    a.GetIdx()
+                    for a in rdmol.GetAtoms()
+                    if a.GetAtomicNum() != 1 and a.GetMass() != 1
+                )
+            }
+            rdmol = rdkit.Chem.RemoveHs(rdmol, updateExplicitCount=True)
 
-    # Process highlight_bonds argument for highlightBonds and highlightBondColors
-    # highlightBonds takes a list of bond indices
-    highlight_bond_indices: list[int]
-    # highlightBondColors takes a mapping from bond indices to colors
-    highlight_bond_colors: dict[int, Color] | None
-    if highlight_bonds is None:
-        highlight_bond_indices = []
-        highlight_bond_colors = None
-    elif isinstance(highlight_bonds, dict):
-        highlight_bond_colors = {
-            rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b]).GetIdx(): tuple(v)
-            for (i_a, i_b), v in highlight_bonds.items()
-            if i_a in idx_map and i_b in idx_map
-        }
+        # Process highlight_atoms argument for highlightAtoms and highlightAtomColors
+        # highlightAtoms takes a list of atom indices
+        # highlightAtomColors takes a mapping from atom indices to colors
+        if highlight_atoms is None:
+            highlight_atoms = []
+            highlight_atom_colors = None
+        elif isinstance(highlight_atoms, dict):
+            highlight_atom_colors = {
+                idx_map[i]: tuple(c) for i, c in highlight_atoms.items() if i in idx_map
+            }
+            highlight_atoms = list(highlight_atoms.keys())
+        else:
+            highlight_atoms = [idx_map[i] for i in highlight_atoms if i in idx_map]
+            highlight_atom_colors = None
 
-        highlight_bond_indices = list(highlight_bond_colors.keys())
-    else:
-        highlight_bond_indices = [
-            rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b]).GetIdx()
-            for i_a, i_b in highlight_bonds
-            if i_a in idx_map and i_b in idx_map
-        ]
-        highlight_bond_colors = None
+        # Process highlight_bonds argument for highlightBonds and highlightBondColors
+        # highlightBonds takes a list of bond indices
+        highlight_bond_indices: list[int]
+        # highlightBondColors takes a mapping from bond indices to colors
+        highlight_bond_colors: dict[int, Color] | None
+        if highlight_bonds is None:
+            highlight_bond_indices = []
+            highlight_bond_colors = None
+        elif isinstance(highlight_bonds, dict):
+            highlight_bond_colors = {
+                rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b]).GetIdx(): tuple(v)
+                for (i_a, i_b), v in highlight_bonds.items()
+                if i_a in idx_map and i_b in idx_map
+            }
 
-    # Process bond_notes argument and place notes in the molecule
-    if bond_notes is not None:
-        for (i_a, i_b), note in bond_notes.items():
-            if i_a not in idx_map or i_b not in idx_map:
-                continue
-            rdbond = rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b])
-            rdbond.SetProp("bondNote", asciify(str(note)))
+            highlight_bond_indices = list(highlight_bond_colors.keys())
+        else:
+            highlight_bond_indices = [
+                rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b]).GetIdx()
+                for i_a, i_b in highlight_bonds
+                if i_a in idx_map and i_b in idx_map
+            ]
+            highlight_bond_colors = None
 
-    # Process atom_notes argument and place notes in the molecule
-    if atom_notes is not None:
-        for i, note in atom_notes.items():
-            if i not in idx_map:
-                continue
-            rdatom = rdmol.GetAtomWithIdx(idx_map[i])
-            rdatom.SetProp("atomNote", asciify(str(note)))
+        # Process bond_notes argument and place notes in the molecule
+        if bond_notes is not None:
+            for (i_a, i_b), note in bond_notes.items():
+                if i_a not in idx_map or i_b not in idx_map:
+                    continue
+                rdbond = rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b])
+                rdbond.SetProp("bondNote", asciify(str(note)))
 
-    # Resolve kekulization so it is the same for all drawn molecules
-    rdkit.Chem.rdmolops.Kekulize(rdmol, clearAromaticFlags=True)
+        # Process atom_notes argument and place notes in the molecule
+        if atom_notes is not None:
+            for i, note in atom_notes.items():
+                if i not in idx_map:
+                    continue
+                rdatom = rdmol.GetAtomWithIdx(idx_map[i])
+                rdatom.SetProp("atomNote", asciify(str(note)))
 
-    # Compute 2D coordinates
-    rdkit.Chem.rdDepictor.Compute2DCoords(rdmol)
+        # Resolve kekulization so it is the same for all drawn molecules
+        rdkit.Chem.rdmolops.Kekulize(rdmol, clearAromaticFlags=True)
 
-    # Construct the drawing object and get a handle to its options
-    drawer = rdkit.Chem.Draw.MolDraw2DSVG(width, height)
-    draw_options = drawer.drawOptions()
+        # Compute 2D coordinates
+        rdkit.Chem.rdDepictor.Compute2DCoords(rdmol)
 
-    # Specify the scale to fit all atoms
-    # This is important for emphasize_atoms
-    coords_2d = next(iter(rdmol.GetConformers())).GetPositions()[..., (0, 1)]
-    drawer.SetScale(
-        width,
-        height,
-        rdkit.Geometry.rdGeometry.Point2D(*(coords_2d.min(axis=0) - 1.0)),
-        rdkit.Geometry.rdGeometry.Point2D(*(coords_2d.max(axis=0) + 1.0)),
-    )
+        # Construct the drawing object and get a handle to its options
+        drawer = rdkit.Chem.Draw.MolDraw2DSVG(width, height)
+        draw_options = drawer.drawOptions()
 
-    # Set the colors used for each element according to the emphasize_atoms and
-    # color_by_element arguments
-    if emphasize_atoms:
-        draw_options.setAtomPalette(
-            {i: (0.8, 0.8, 0.8) for i in range(rdmol.GetNumAtoms())},
+        # Specify the scale to fit all atoms
+        # This is important for emphasize_atoms
+        coords_2d = next(iter(rdmol.GetConformers())).GetPositions()[..., (0, 1)]
+        drawer.SetScale(
+            width,
+            height,
+            rdkit.Geometry.rdGeometry.Point2D(*(coords_2d.min(axis=0) - 1.0)),
+            rdkit.Geometry.rdGeometry.Point2D(*(coords_2d.max(axis=0) + 1.0)),
         )
-    else:
-        set_atom_palette(draw_options)
 
-    # Draw the molecule
-    # Note that if emphasize_atoms is used, this will be the un-emphasized parts
-    # of the molecule
-    drawer.DrawMolecule(
-        rdmol,
-        highlightAtoms=highlight_atoms,
-        highlightAtomColors=highlight_atom_colors,
-        highlightBonds=highlight_bond_indices,
-        highlightBondColors=highlight_bond_colors,
-    )
+        # Set the colors used for each element according to the emphasize_atoms and
+        # color_by_element arguments
+        if emphasize_atoms:
+            draw_options.setAtomPalette(
+                {i: (0.8, 0.8, 0.8) for i in range(rdmol.GetNumAtoms())},
+            )
+        else:
+            set_atom_palette(draw_options)
 
-    # Draw an overlapping molecule for the emphasized atoms
-    if emphasize_atoms:
-        # Set the atom palette according to the color_by_element argument
-        set_atom_palette(draw_options)
+        # Draw the molecule
+        # Note that if emphasize_atoms is used, this will be the un-emphasized parts
+        # of the molecule
+        drawer.DrawMolecule(
+            rdmol,
+            highlightAtoms=highlight_atoms,
+            highlightAtomColors=highlight_atom_colors,
+            highlightBonds=highlight_bond_indices,
+            highlightBondColors=highlight_bond_colors,
+        )
 
-        # Create a copy of the molecule that removes atoms that aren't emphasized
-        emphasized = rdkit.Chem.rdchem.RWMol(rdmol)
-        emphasized.BeginBatchEdit()
-        for i in set(idx_map) - set(emphasize_atoms):
-            emphasized.RemoveAtom(idx_map[i])
-        emphasized.CommitBatchEdit()
+        # Draw an overlapping molecule for the emphasized atoms
+        if emphasize_atoms:
+            # Set the atom palette according to the color_by_element argument
+            set_atom_palette(draw_options)
 
-        # Draw the molecule. The scale has been fixed and we're re-using the
-        # same coordinates, so this will overlap the background molecule
-        drawer.DrawMolecule(emphasized)
+            # Create a copy of the molecule that removes atoms that aren't emphasized
+            emphasized = rdkit.Chem.rdchem.RWMol(rdmol)
+            emphasized.BeginBatchEdit()
+            for i in set(idx_map) - set(emphasize_atoms):
+                emphasized.RemoveAtom(idx_map[i])
+            emphasized.CommitBatchEdit()
 
-    # Finalize the SVG
-    drawer.FinishDrawing()
+            # Draw the molecule. The scale has been fixed and we're re-using the
+            # same coordinates, so this will overlap the background molecule
+            drawer.DrawMolecule(emphasized)
 
-    # Return an SVG object that we can view in notebook
-    svg_contents = drawer.GetDrawingText()
-    return SVG(svg_contents)
+        # Finalize the SVG
+        drawer.FinishDrawing()
+
+        # Return an SVG object that we can view in notebook
+        svg_contents = drawer.GetDrawingText()
+        return SVG(svg_contents)
+
+    return inner()
